ea406375d2905c7b9f46348daa40c5775957b919
Rob <szotrj@gmail.com>
2021-02-21 23:42:10 -0500

    Update __main__.py

--- src/__main__.py
+++ src/__main__.py
@@ -1,6 +1,11 @@
 import parliament
 
-analyzed_policy = analyze_policy_string(policy_doc)
-for f in analyzed_policy.findings:
-  ef = parliament.enhance_finding(f)
-  print(vars(ef))
+
+def analyze_policy(policy_doc):
+    analyzed_policy = analyze_policy_string(policy_doc)
+    for f in analyzed_policy.findings:
+        ef = parliament.enhance_finding(f)
+        return vars(ef)
+
+if __name__ == "__main__":
+    analyze_policy(policy)
