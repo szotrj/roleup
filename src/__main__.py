391f5f641838b4a87eb778db041034de6207e782
Rob <szotrj@gmail.com>
2021-02-21 23:49:47 -0500

    Update __main__.py

--- src/__main__.py
+++ src/__main__.py
@@ -1,11 +1,27 @@
 import parliament
 
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+    #
+    # ... configure command line arguments ...
+    #
+    return parser.parse_args()
+
+def do_stuff(args):
+    #
+    # ... main functionality goes in here ...
+    #
+
+def main():
+    args = parse_args()
+    analyze_policy(args)
+
 def analyze_policy(policy_doc):
-    analyzed_policy = analyze_policy_string(policy_doc)
+    analyzed_policy = parliament.analyze_policy_string(policy_doc)
     for f in analyzed_policy.findings:
         ef = parliament.enhance_finding(f)
         return vars(ef)
 
 if __name__ == "__main__":
-    analyze_policy(policy)
+    main()
